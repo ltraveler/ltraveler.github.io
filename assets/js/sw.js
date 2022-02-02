19bdfef66afeb8f1b504be689d2687bd53b50f20
zeusdev <zeusdev@tuta.io>
2022-02-02 00:54:32 -0500

    latest one

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.1.2--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2022-01-27T14:46:43-05:00--sw/";
+const CONTENT_CACHE = "content--2022-02-02T00:50:44-05:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
